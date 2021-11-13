5548ac8b69f36545cb61380f5dae4121d552572e
keefelee <keefeleedism@gmail.com>
2021-11-13 17:46:35 +0800

    updated homepage with talk

--- src/homepage/homepage.ino
+++ src/homepage/homepage.ino
@@ -2,10 +2,25 @@
 #include <SPI.h>
 #include <Wire.h>
 #include "Bitmaps.h"
+#include "BMA250.h"
+
+typedef struct {
+  int width;
+  int height;
+  const  unsigned int *bitmap;
+} sprite;
 
 TinyScreen display = TinyScreen(TinyScreenDefault);
-int start_x, start_y; //Wizard spawn coordinates
+BMA250 accel_sensor;
 int wizard_x = 60; //Temporary wizard location, later to use global x coordinate var
+int gravity = 1; // Determines if screen if flipped or not, 1 is flipped 0 is not flipped
+int x_accel = 0; //Accelerometer value for x axis
+int backgroundColor = TS_16b_Black;
+//Sprite initialisation
+sprite wizard = {34,54,wizardBitmap};
+sprite wizard_flipped = {34,54,wizardBitmap_flipped};
+//sprite crystal_ball = {20,20,crystalBallBitmap};
+
 
 void setup() {
   randomSeed(analogRead(0)); // Make it more random
@@ -15,9 +30,12 @@ void setup() {
   display.clearScreen();
   display.setBitDepth(TSBitDepth16);
   display.setColorMode(TSColorModeRGB);  
-  display.setFlip(1);
-  
+  display.setFlip(gravity);
+  uint16_t time = millis();
+  // Set up the BMA250 acccelerometer sensor
+  accel_sensor.begin(BMA250_range_2g, BMA250_update_time_32ms); 
 }
+
 
 void loop() {
   display.clearScreen();
@@ -32,39 +50,18 @@ void loop() {
   delay(5000);
   display.clearWindow(0,0,96,30);
   
-//  //y is vertical, x is horizontal. bottom left is origin
-//  for (int i = 60; i > 10; i-=20) {
-//    drawMap(wizardBitmap,34,54,i,30);
-//    delay(500);
-//  }
-//
-//  display.setCursor(3,10);
-//  display.print("I predict that...");
-//  delay(1000);
-//  display.setCursor(3,20);
-//  display.print("u gey");
-//  delay(3000);
-//  display.clearScreen();
-//  display.setMirror(1);
-//  for (int i = 60; i > 10; i-=20) {
-//    drawMap(wizardBitmap,34,54,i,30);
-//    delay(500);
-//  }
-//  display.clearScreen();
-//  display.setMirror(0);
-
   // Here is the infinite loop for idle, change to for loop if want limit loops.
   while (1){
 //    display.clearScreen();
 //    display.setCursor(3,10);
 //    display.print(i);
+    accel_sensor.read();//This function gets new data from the acccelerometer
+    x_accel = accel_sensor.X;
+    gravity_check();
     idle();
-  }
-
-  
-  
-  
+  }  
 }
+
 
 void drawMap(unsigned int* bitmap,int x_len,int y_len,int start_x,int start_y) {
   display.goTo(start_x, start_y);
@@ -76,13 +73,14 @@ void drawMap(unsigned int* bitmap,int x_len,int y_len,int start_x,int start_y) {
   }
 }
 
-void idle(){
-  //Currently used for walk, later will be converted to 
+
+//Currently used for walking
+void idle(){  
   int max_walk = 0, rand_walk =0, bound = 0;
   
   //Get wizards coordinates, decide how far can he walk in either direction
   int walk_direction = random(2); //0 = left, 1 = right
-  if (walk_direction == 0){ //Walk left
+  if (walk_direction == 0){                                    //Walk left
     max_walk = wizard_x / 5;
     rand_walk = random(max_walk);
     bound = wizard_x - (rand_walk * 5);
@@ -99,10 +97,11 @@ void idle(){
     for (wizard_x; wizard_x > bound; wizard_x -= 5) {
       //display.setCursor(45,10);
       //display.print(wizard_x);
-      drawMap(wizardBitmap,34,54,wizard_x,30);
+      //drawMap(wizardBitmap,34,54,wizard_x,30);
+      drawBuffer(wizard, wizard_x, 30);
       delay(500);
     }
-  } else { //Walk Right
+  } else {                                                     //Walk Right
     max_walk = (80 - wizard_x) / 5;
     rand_walk = random(max_walk);
     //display.setCursor(15,10);
@@ -128,14 +127,9 @@ void idle(){
     talk();
 }
 
+// Fn to make him say random things
 void talk() {
-  // Fn to make him say random things
   int rand_word = random(5);
-//  display.setCursor(3,10);
-//  display.print("NEW LOOP");
-//  display.setCursor(3,20);
-//  display.print(rand_word);
-//  delay(1000);
   switch(rand_word) {
     case 0:
       display.setCursor(0,10);
@@ -183,4 +177,52 @@ void talk() {
       display.clearWindow(0,0,96,30);
       break;
   }
+}
+
+void gravity_check() {
+  // This function flips the display, and plays a falling animation before continuing idle
+  if (x_accel == -1) {
+    // Print error message to Serial Monitor
+    display.print("ERROR! NO BMA250 DETECTED!");
+  }
+
+  if (x_accel <= -200 && gravity != 1) {
+    gravity = 1;
+    display.setFlip(gravity);
+  } else if (x_accel >= 200 && gravity != 0) {
+    gravity = 0;
+    display.setFlip(gravity);
+  }
+  
+}
+
+// drawBuffer function draws a bitmap at the start_x and the start_y provided.
+void drawBuffer(sprite bitmap, int start_x, int start_y) {
+  uint8_t lineBuffer[96 * 64 * 3];
+  display.startData();
+  for (int y = 0; y < 64; y++) {
+    for (int b = 0; b < 96; b++) {
+      lineBuffer[b * 2] = backgroundColor >> 8;
+      lineBuffer[b * 2 + 1] = backgroundColor;
+    }
+    if (y >= start_y && y < start_y + bitmap.height) {
+      int endX = start_x + bitmap.width;
+      if (start_x < 96 && endX > 0) {
+        int xBitmapOffset = 0;
+        int xStart = 0;
+        if (start_x < 0) xBitmapOffset -= start_x;
+        if (start_x > 0) xStart = start_x;
+        int yBitmapOffset = (y - start_y) * bitmap.width;
+        for (int x = xStart; x < endX; x++) {
+          unsigned int color = bitmap.bitmap[xBitmapOffset + yBitmapOffset++];
+          if (color != backgroundColor) {
+            lineBuffer[(x) * 2] = color >> 8;
+            lineBuffer[(x) * 2 + 1] = color;
+          }
+        }
+      }
+    }
+    display.writeBuffer(lineBuffer, 96 * 2);
+  }
+  display.endTransfer();
 }
