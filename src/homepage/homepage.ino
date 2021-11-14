5c06c6a79e7c71650c451a5cfefd5e8f8731d435
keefelee <keefeleedism@gmail.com>
2021-11-14 16:54:56 +0800

    basic homepage anims done

--- src/homepage/homepage.ino
+++ src/homepage/homepage.ino
@@ -3,12 +3,7 @@
 #include <Wire.h>
 #include "Bitmaps.h"
 #include "BMA250.h"
-
-typedef struct {
-  int width;
-  int height;
-  const  unsigned int *bitmap;
-} sprite;
+//#include "drawSprites.h"
 
 TinyScreen display = TinyScreen(TinyScreenDefault);
 BMA250 accel_sensor;
@@ -16,10 +11,6 @@ int wizard_x = 60; //Temporary wizard location, later to use global x coordinate
 int gravity = 1; // Determines if screen if flipped or not, 1 is flipped 0 is not flipped
 int x_accel = 0; //Accelerometer value for x axis
 int backgroundColor = TS_16b_Black;
-//Sprite initialisation
-sprite wizard = {34,54,wizardBitmap};
-sprite wizard_flipped = {34,54,wizardBitmap_flipped};
-//sprite crystal_ball = {20,20,crystalBallBitmap};
 
 
 void setup() {
@@ -39,9 +30,8 @@ void setup() {
 
 void loop() {
   display.clearScreen();
-  drawMap(wizardBitmap,34,54,60,30);
+  drawBuffer(wizard,60,30);
   display.setFont(thinPixel7_10ptFontInfo);
-  //display.setFont(liberationSansNarrow_8ptFontInfo);
   display.fontColor(TS_8b_Green,TS_8b_Black);
   display.setCursor(0,10);
   display.print("Why hello there...");
@@ -63,17 +53,6 @@ void loop() {
 }
 
 
-void drawMap(unsigned int* bitmap,int x_len,int y_len,int start_x,int start_y) {
-  display.goTo(start_x, start_y);
-  for (int i=0; i<x_len*y_len; i++){
-    if (i%x_len == 0){
-      display.goTo(start_x,start_y+(i/y_len));
-    }
-    display.writePixel(bitmap[i]);
-  }
-}
-
-
 //Currently used for walking
 void idle(){  
   int max_walk = 0, rand_walk =0, bound = 0;
@@ -89,8 +68,8 @@ void idle(){
     if (bound >= wizard_x) {
       display.setCursor(20,10);
       display.print("*zzzzzzzz*");
-      display.clearWindow(0,0,96,30);
       delay(5000);
+      display.clearWindow(0,0,96,30);
       return;
     }
  
@@ -99,7 +78,7 @@ void idle(){
       //display.print(wizard_x);
       //drawMap(wizardBitmap,34,54,wizard_x,30);
       drawBuffer(wizard, wizard_x, 30);
-      delay(500);
+      delay(1000);
     }
   } else {                                                     //Walk Right
     max_walk = (80 - wizard_x) / 5;
@@ -119,8 +98,9 @@ void idle(){
     for (wizard_x; wizard_x < bound; wizard_x += 5) {
       //display.setCursor(45,10);
       //display.print(wizard_x);
-      drawMap(wizardBitmap_flipped,34,54,wizard_x,30);
-      delay(500);
+      //drawMap(wizardBitmap_flipped,34,54,wizard_x,30);
+      drawBuffer(wizard_flipped, wizard_x, 30);
+      delay(1000);
     }
   }
   //Make him talk
@@ -196,9 +176,41 @@ void gravity_check() {
   
 }
 
+//// drawBuffer function draws a bitmap at the start_x and the start_y provided.
+//void drawBuffer(sprite bitmap, int start_x, int start_y) {
+//  uint8_t lineBuffer[96 * 64 * 3];
+//  display.startData();
+//  for (int y = 0; y < 64; y++) {
+//    for (int b = 0; b < 96; b++) {
+//      lineBuffer[b * 2] = backgroundColor >> 8;
+//      lineBuffer[b * 2 + 1] = backgroundColor;
+//    }
+//    if (y >= start_y && y < start_y + bitmap.height) {
+//      int endX = start_x + bitmap.width;
+//      if (start_x < 96 && endX > 0) {
+//        int xBitmapOffset = 0;
+//        int xStart = 0;
+//        if (start_x < 0) xBitmapOffset -= start_x;
+//        if (start_x > 0) xStart = start_x;
+//        int yBitmapOffset = (y - start_y) * bitmap.width;
+//        for (int x = xStart; x < endX; x++) {
+//          unsigned int color = bitmap.bitmap[xBitmapOffset + yBitmapOffset++];
+//          if (color != backgroundColor) {
+//            lineBuffer[(x) * 2] = color >> 8;
+//            lineBuffer[(x) * 2 + 1] = color;
+//          }
+//        }
+//      }
+//    }
+//    display.writeBuffer(lineBuffer, 96 * 2);
+//  }
+//  display.endTransfer();
+//}
+
 // drawBuffer function draws a bitmap at the start_x and the start_y provided.
 void drawBuffer(sprite bitmap, int start_x, int start_y) {
-  uint8_t lineBuffer[96 * 64 * 3];
+  uint8_t lineBuffer[96 * 64 * 1];
+  display.goTo(0,0);
   display.startData();
   for (int y = 0; y < 64; y++) {
     for (int b = 0; b < 96; b++) {
