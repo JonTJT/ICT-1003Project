6012a7a5e3987468acbe64af918192f72a3498f0
keefelee <keefeleedism@gmail.com>
2021-11-19 17:21:00 +0800

    homepage uses drawbuffer and gravity

--- src/homepage/homepage.ino
+++ src/homepage/homepage.ino
@@ -3,14 +3,14 @@
 #include <Wire.h>
 #include "Bitmaps.h"
 #include "BMA250.h"
-//#include "drawSprites.h"
 
 TinyScreen display = TinyScreen(TinyScreenDefault);
+#include "drawSprites.h"
+
 BMA250 accel_sensor;
-int wizard_x = 60; //Temporary wizard location, later to use global x coordinate var
+int wizard_x = 45; //Temporary wizard location, later to use global x coordinate var
 int gravity = 1; // Determines if screen if flipped or not, 1 is flipped 0 is not flipped
 int x_accel = 0; //Accelerometer value for x axis
-int backgroundColor = TS_16b_Black;
 
 
 void setup() {
@@ -30,7 +30,7 @@ void setup() {
 
 void loop() {
   display.clearScreen();
-  drawBuffer(wizard,60,30);
+  drawBuffer(wizard,wizard_x,30);
   display.setFont(thinPixel7_10ptFontInfo);
   display.fontColor(TS_8b_Green,TS_8b_Black);
   display.setCursor(0,10);
@@ -42,11 +42,6 @@ void loop() {
   
   // Here is the infinite loop for idle, change to for loop if want limit loops.
   while (1){
-//    display.clearScreen();
-//    display.setCursor(3,10);
-//    display.print(i);
-    accel_sensor.read();//This function gets new data from the acccelerometer
-    x_accel = accel_sensor.X;
     gravity_check();
     idle();
   }  
@@ -63,43 +58,33 @@ void idle(){
     max_walk = wizard_x / 5;
     rand_walk = random(max_walk);
     bound = wizard_x - (rand_walk * 5);
-    //display.setCursor(30,10);
-    //display.print(bound);
     if (bound >= wizard_x) {
       display.setCursor(20,10);
       display.print("*zzzzzzzz*");
-      delay(5000);
+      delay(2000);
       display.clearWindow(0,0,96,30);
       return;
     }
  
     for (wizard_x; wizard_x > bound; wizard_x -= 5) {
-      //display.setCursor(45,10);
-      //display.print(wizard_x);
-      //drawMap(wizardBitmap,34,54,wizard_x,30);
       drawBuffer(wizard, wizard_x, 30);
+      gravity_check();
       delay(1000);
     }
   } else {                                                     //Walk Right
     max_walk = (80 - wizard_x) / 5;
     rand_walk = random(max_walk);
-    //display.setCursor(15,10);
-    //display.print(rand_walk);
     bound = wizard_x + rand_walk * 5;
-    //display.setCursor(30,10);
-    //display.print(bound);
     if (bound <= wizard_x) {
       display.setCursor(15,10);
       display.print("*lalalalala*");
-      delay(5000);
+      delay(2000);
       display.clearWindow(0,0,96,30);
       return;
     }
     for (wizard_x; wizard_x < bound; wizard_x += 5) {
-      //display.setCursor(45,10);
-      //display.print(wizard_x);
-      //drawMap(wizardBitmap_flipped,34,54,wizard_x,30);
       drawBuffer(wizard_flipped, wizard_x, 30);
+      gravity_check();
       delay(1000);
     }
   }
@@ -159,82 +144,61 @@ void talk() {
   }
 }
 
+
 void gravity_check() {
   // This function flips the display, and plays a falling animation before continuing idle
+  int dist_from_centre_xaxis = 0;
+  accel_sensor.read();//This function gets new data from the acccelerometer
+  x_accel = accel_sensor.X;
   if (x_accel == -1) {
     // Print error message to Serial Monitor
     display.print("ERROR! NO BMA250 DETECTED!");
   }
 
+   // Check if flipped already
   if (x_accel <= -200 && gravity != 1) {
     gravity = 1;
-    display.setFlip(gravity);
   } else if (x_accel >= 200 && gravity != 0) {
     gravity = 0;
-    display.setFlip(gravity);
+  } else {
+    return;
   }
+
+    falling();    
+    display.setFlip(gravity);    
+    display.clearScreen();
+
+    //Mirror x value across centre for flip
+    //dist_from_centre_xaxis = 48 - wizard_x;
+    //wizard_x = wizard_x - (2 * dist_from_centre_xaxis);
+    
+    drawBuffer(wizard, wizard_x, 30);
+    delay(1000);
+    display.setCursor(0,10);
+    display.print("Uh...RUDE!?");
+    delay(3000);
+    display.setCursor(0,20);
+    display.print("That hurt T-T");
+    delay(3000);
+    display.clearWindow(0,0,96,30);
   
 }
 
-//// drawBuffer function draws a bitmap at the start_x and the start_y provided.
-//void drawBuffer(sprite bitmap, int start_x, int start_y) {
-//  uint8_t lineBuffer[96 * 64 * 3];
-//  display.startData();
-//  for (int y = 0; y < 64; y++) {
-//    for (int b = 0; b < 96; b++) {
-//      lineBuffer[b * 2] = backgroundColor >> 8;
-//      lineBuffer[b * 2 + 1] = backgroundColor;
-//    }
-//    if (y >= start_y && y < start_y + bitmap.height) {
-//      int endX = start_x + bitmap.width;
-//      if (start_x < 96 && endX > 0) {
-//        int xBitmapOffset = 0;
-//        int xStart = 0;
-//        if (start_x < 0) xBitmapOffset -= start_x;
-//        if (start_x > 0) xStart = start_x;
-//        int yBitmapOffset = (y - start_y) * bitmap.width;
-//        for (int x = xStart; x < endX; x++) {
-//          unsigned int color = bitmap.bitmap[xBitmapOffset + yBitmapOffset++];
-//          if (color != backgroundColor) {
-//            lineBuffer[(x) * 2] = color >> 8;
-//            lineBuffer[(x) * 2 + 1] = color;
-//          }
-//        }
-//      }
-//    }
-//    display.writeBuffer(lineBuffer, 96 * 2);
-//  }
-//  display.endTransfer();
-//}
-
-// drawBuffer function draws a bitmap at the start_x and the start_y provided.
-void drawBuffer(sprite bitmap, int start_x, int start_y) {
-  uint8_t lineBuffer[96 * 64 * 1];
-  display.goTo(0,0);
-  display.startData();
-  for (int y = 0; y < 64; y++) {
-    for (int b = 0; b < 96; b++) {
-      lineBuffer[b * 2] = backgroundColor >> 8;
-      lineBuffer[b * 2 + 1] = backgroundColor;
-    }
-    if (y >= start_y && y < start_y + bitmap.height) {
-      int endX = start_x + bitmap.width;
-      if (start_x < 96 && endX > 0) {
-        int xBitmapOffset = 0;
-        int xStart = 0;
-        if (start_x < 0) xBitmapOffset -= start_x;
-        if (start_x > 0) xStart = start_x;
-        int yBitmapOffset = (y - start_y) * bitmap.width;
-        for (int x = xStart; x < endX; x++) {
-          unsigned int color = bitmap.bitmap[xBitmapOffset + yBitmapOffset++];
-          if (color != backgroundColor) {
-            lineBuffer[(x) * 2] = color >> 8;
-            lineBuffer[(x) * 2 + 1] = color;
-          }
-        }
-      }
-    }
-    display.writeBuffer(lineBuffer, 96 * 2);
+// This functions animates the wiz to be falling
+void falling() {
+  for (int i = 30; i >= -15; i -= 5) {
+    drawBuffer(wizard, wizard_x, i);
+    delay(100);
   }
-  display.endTransfer();
+  delay(1000);
+  drawBuffer(wizard, wizard_x + 3, -15);
+  delay(500);
+  drawBuffer(wizard, wizard_x - 3, -15);
+  delay(500);
+  drawBuffer(wizard, wizard_x + 3, -15);
+  delay(500);
+  drawBuffer(wizard, wizard_x - 3, -15);
+  
+
+  delay(3000);
 }
