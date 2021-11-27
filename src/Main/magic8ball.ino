b7dd0efcb24812ad78491bf40677efc67bacdf50
Xynorimas <23660665+Xynorimas@users.noreply.github.com>
2021-11-27 17:42:26 +0800

    updated the fix wonkiness v2

--- src/Main/magic8ball.ino
+++ src/Main/magic8ball.ino
@@ -13,9 +13,16 @@ void Ask8Ball() {
     display.setCursor(5,5);
     drawBuffer(eightBall,63,30);//sprite character
     display.print("Magic 8-Ball");
-    delay(2000);
-    display.clearWindow(0,0,96,64);
-    mainLoop();
+    delay(500);
+    while (1) {
+      if (display.getButtons() == TSButtonUpperLeft || display.getButtons() == TSButtonUpperRight || display.getButtons() == TSButtonLowerLeft) {
+        mainLoop();
+        break;
+      } 
+      else if (display.getButtons() == TSButtonLowerRight) {
+        break;
+      }
+    }
     display.clearScreen();
 }
 
@@ -23,6 +30,8 @@ void mainLoop() {
 //  display.setFont(liberationSans_10ptFontInfo); //font size is 8
   //display.fontColor(WHITE,BLACK);
 
+  display.clearWindow(0,0,96,64);
+  
   while(1) {
     rando = random(0, 17); //You need a random range 1 more than the number of statements you have
     rando = round(rando); //Round it to a whole number
@@ -31,23 +40,23 @@ void mainLoop() {
     int z = accel.Z;
     SerialMonitorInterface.println(z); //printing out Z values for debugging purposes
 
-    if (z < 170) {
+    if (z < 220) {
       drawBuffer(eightBall,63,30);//sprite character 
       display.setCursor(3,5);
       display.print("Shake");
       display.setCursor(3,15);
       display.print("Shake");
       display.setCursor(3,25);
-      display.print("Shake");
+      display.print("Shake"); 
       delay(500);
       display.clearWindow(0,0,96,64);
     }
-    else if (z > 170) {
+    if (z > 220) {
       display.setCursor(5,5); 
       drawBuffer(wizard,63,30);//sprite character
       display.print(phrases[rando]); //Here's the message that gets printed
       SerialMonitorInterface.println(rando);
-      while (accel.Z > 160) {
+      while (accel.Z > 0) {
         for (int i = 0; i < 100; i++) {
           if (display.getButtons() == TSButtonLowerRight) {
             return;
